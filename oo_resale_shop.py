07daf279f66c2ac337e30f1fd613a3e0ebd9e935
Sophia Kwak <sskwak@smith.edu>
2024-09-12 15:03:11 -0400

    computer inventory object

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,30 @@
 class ResaleShop:
 
     # What attributes will it need?
+    from typing import Dict, Optional
+    import computer 
+
+    inventory: Dict[int, Dict]
+    itemID: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = {}
+        self.itemID = 0
 
     # What methods will you need?
+    def buy(self, computer: Dict):
+        self.itemID += 1
+        self.inventory[self.itemID] = computer
+        return self.itemID
+    
+    def sell(self, itemID : int):
+        if itemID in self.inventory:
+            del self.inventory[itemID]
+            print("Item", itemID, "sold!")
+        else: 
+            print("Item", itemID, "not found. Please select another item to sell.")
+
+
+    
